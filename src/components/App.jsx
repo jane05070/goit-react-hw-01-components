18ffdd8dba1f71d1b2d477ac71ff92150a8fa75f
jane05070 <jane05070@gmail.com>
2023-04-28 19:37:03 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
+
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  padding: 20,
+  color: '#010101',
+};
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={styles}>
+      <Profile {...user} />
     </div>
   );
 };
